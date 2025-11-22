df09db5e549587fd577c75c22b0e640f2dfb7fb2
raschy <raschy@gmx.de>
2025-11-22 09:22:54 +0100

    dependabot nov.25

--- build/main.js
+++ build/main.js
@@ -15,13 +15,23 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 /*
  * Created with @iobroker/create-adapter v2.6.5
@@ -231,7 +241,7 @@ class Airquality extends utils.Adapter {
                     'zh-cn': '计量类型数目',
                 },
                 type: 'number',
-                role: 'value',
+                role: 'value.max',
                 unit: '',
                 read: true,
                 write: false,
@@ -262,7 +272,7 @@ class Airquality extends utils.Adapter {
                     'zh-cn': '上次测量的时间',
                 },
                 type: 'string',
-                role: 'text',
+                role: 'date.start',
                 unit: '',
                 read: true,
                 write: false,
